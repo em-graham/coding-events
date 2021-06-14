0cf33d99f0796cbc2a0610bc9e6e1049b2a0f73e
Emilee Graham <emgraham@Emilees-MacBook-Air-2.local>
2021-06-14 12:23:06 +0100

    Completed video 1.3

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,8 +2,10 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,14 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    // it's OK to just put List first if specified as ArrayList second
+    // Also, you don't need to type <String> again second if List specifies this
+    private static List<String> events = new ArrayList<>();
+
+
     // lives at /events
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple MMDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,4 +31,12 @@ public class EventController {
     public String renderCreateEventForm() {
         return "events/create";
     }
+
+    // lives at /events/create. OK to be same as above b/c they handle different requests
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; // redirect to root path /events
+    }
+
 }
